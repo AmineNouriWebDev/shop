381d6d42957f97f942439cc7ed9844f8eb350b32
Amine Nouri <nouri.webdev@gmail.com>
2026-03-03 04:03:43 +0100

    security: Fix XSS vulnerabilities — htmlspecialchars on all user-supplied search output

--- includes/banniere.php
+++ includes/banniere.php
@@ -8,15 +8,15 @@
 		<div class="view intro-2 banniere" style="background: url('dist/img/iptv-definition.png') no-repeat center center;background-size: cover;" >	
 		    
 		    <div class="banner-title text-center white-text p-5">
-				<?php if((isset($_POST['action']) && $_POST['action'] == 'search')){ ?>
-		        <h1> Produits de la recherche : <?php echo $_POST['recherche']; ?></h1>
-				<?php }elseif((isset($_GET['marque']) && $_GET['marque'] != '')){ ?>
-		        <h1> Produits de la recherche :  <?php echo $_GET['marque']; ?></h1>
-				<?php }elseif( (isset($_GET['search']) && $_GET['search'] != '')){ ?>
-		        <h1> Produits de la recherche : <?php echo $_GET['search']; ?></h1>
-				<?php }elseif(isset($titre) && $titre != ''){ ?>
+				<?php if((isset($_POST['action']) && $_POST['action'] == 'search')): ?>
+		        <h1> Produits de la recherche : <?php echo htmlspecialchars($_POST['recherche'] ?? '', ENT_QUOTES, 'UTF-8'); ?></h1>
+				<?php elseif((isset($_GET['marque']) && $_GET['marque'] != '')): ?>
+		        <h1> Produits de la recherche :  <?php echo htmlspecialchars($_GET['marque'], ENT_QUOTES, 'UTF-8'); ?></h1>
+				<?php elseif((isset($_GET['search']) && $_GET['search'] != '')): ?>
+		        <h1> Produits de la recherche : <?php echo htmlspecialchars($_GET['search'], ENT_QUOTES, 'UTF-8'); ?></h1>
+				<?php elseif(isset($titre) && $titre != ''): ?>
 				<h1><?php echo $titre; ?></h1>
-				<?php } ?>
+				<?php endif; ?>
 		    </div>
 			
 		</div>
